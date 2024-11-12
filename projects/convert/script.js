1364b62c1e2f0cab69082ac4ccea8311adc89878
Caio Henrique <caio.hda@gmail.com>
2024-11-12 07:09:23 -0300

    feat/convert
    Validate the amount input
    Get the currency
    Show footer
    Displaying the currency rate

---
+++ projects/convert/script.js
@@ -0,0 +1,50 @@
+// Daily currency rate
+const USD = 5.67
+const EUR = 6.12
+const GBP = 7.37
+
+// Getting elements from document
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+
+// Manipulating the input to recieve only numbers
+amount.addEventListener("input", () => {
+    const hasCharacterRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharacterRegex, "")
+})
+
+// Getting submit event from form
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    switch (currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    }
+}
+
+// Function to convert the currency
+function convertCurrency(amount, price, symbol){
+    try {
+        
+        description.textContent = `${symbol} 1 = R$ ${price}`
+        
+        footer.classList.add("show-result")
+        console.log(description.innerText)
+        
+    } catch (error) {
+        console.log(error)
+        footer.classList.remove("show-result")
+        alert("Error at conversion, try again latter")
+    }
+}
