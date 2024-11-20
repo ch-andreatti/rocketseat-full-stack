d271950bf05c85a08d29ca5a249649858b5b9c73
Caio Henrique <caio.hda@gmail.com>
2024-11-20 08:38:01 -0300

    feat/refund
    Update totals quantity
    Update totals amount
    Remove item at list
    Form clear

--- projects/refund/script.js
+++ projects/refund/script.js
@@ -6,7 +6,8 @@ const amount = document.getElementById("amount")
 
 // Selection of list elements
 const expenseList = document.querySelector("ul")
-const ExpensesQuantity = document.querySelector("aside header p span")
+const expensesQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
 
 // Currency input event
 amount.oninput = () => {
@@ -41,7 +42,6 @@ form.onsubmit = (event) => {
     expenseAdd(newExpense)
 }
 
-// Add new expense
 function expenseAdd(newExpense){
     try {
 
@@ -81,8 +81,8 @@ function expenseAdd(newExpense){
         expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
         expenseList.append(expenseItem)
 
-        // Update totals
         updateTotals()
+        formClear()
         
     } catch (error) {
         alert("Não foi possível atualizar a lista de despesas.")
@@ -90,23 +90,55 @@ function expenseAdd(newExpense){
     }
 }
 
-// Update totals
 function updateTotals(){
     try {
         
         // List quantity
         const items = expenseList.children
-        ExpensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+        expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
 
         // List total
         let total = 0
         for(let item = 0; item < items.length; item++){
             const itemAmount = items[item].querySelector(".expense-amount")
-            console.log(itemAmount)
+            
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",", ".")
+            value = parseFloat(value)
+
+            if (isNaN(value)){
+                return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+            }
+
+            total += Number(value)
         }
+        total = formatCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+
+        expensesTotal.innerHTML = ""
+        expensesTotal.append(symbolBRL, total)
 
     } catch (error) {
         console.log(error)
         alert("Não foi possível atualizar os totais")
     }
+}
+
+// Remove item event
+expenseList.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-icon")){
+        const item = event.target.closest(".expense")
+        item.remove()
+    }
+
+    updateTotals()
+})
+
+function formClear(){
+    expense.value = ""
+    category.value = ""
+    amount.value = ""
+
+    expense.focus()
 }
